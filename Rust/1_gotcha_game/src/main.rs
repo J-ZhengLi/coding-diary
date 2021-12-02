eb58b04c07fc052c8a4fa6049df33321d3a2ec5c
J-ZhengLi <lizhengghengj@gmail.com>
2021-12-02 11:05:20 +0800

    update project1 to use Display trait

--- Rust/1_gotcha_game/src/main.rs
+++ Rust/1_gotcha_game/src/main.rs
@@ -1,35 +1,93 @@
 mod rng_helper;
 
-use std::{fmt, default, io::{self, Write}};
 use rng_helper::random;
+use std::io::{self, Write};
 
 // example database for items with different rarity
 static LEGENDARY_ITEMS: [&str; 15] = [
-    "Mona", "Diluc", "Klee", "Jean", "Venti", "Albedo",
-    "Keqing", "Xiao", "Qiqi", "Zhongli", "Ganyu",
-    "Raiden Shogun", "Yomiya", "Itto", "Kamisato Ayaka"
+    "Mona",
+    "Diluc",
+    "Klee",
+    "Jean",
+    "Venti",
+    "Albedo",
+    "Keqing",
+    "Xiao",
+    "Qiqi",
+    "Zhongli",
+    "Ganyu",
+    "Raiden Shogun",
+    "Yomiya",
+    "Itto",
+    "Kamisato Ayaka",
 ];
 static EPIC_ITEMS: [&str; 20] = [
-    "Barbara", "Amber", "Bennet", "Sucross", "Razor",
-    "Xiangling", "Xingqiu", "Chongyun", "Fischle", "Xinyan",
-    "Yanfei", "Ningguang",
-    "Black Sword", "The Flute", "Stringless", "Rust", "Prototype Sword",
-    "Prototype Great Sword", "Prototype Bow", "Prototype Spear"
+    "Barbara",
+    "Amber",
+    "Bennet",
+    "Sucross",
+    "Razor",
+    "Xiangling",
+    "Xingqiu",
+    "Chongyun",
+    "Fischle",
+    "Xinyan",
+    "Yanfei",
+    "Ningguang",
+    "Black Sword",
+    "The Flute",
+    "Stringless",
+    "Rust",
+    "Prototype Sword",
+    "Prototype Great Sword",
+    "Prototype Bow",
+    "Prototype Spear",
 ];
 static RARE_ITEMS: [&str; 15] = [
-    "Black Steel Set - Head", "Black Steel Set - Body", "Black Steel Set - Torso",
-    "Black Steel Set - Foot", "Black Steel Set - Cape", "Black Steel Sword",
-    "Black Steel Shield", "Black Steel Sword", "Ruby Necklace - ATK%", "Sapphire Ring - MAGIC%",
-    "Moonlight Wand", "The Invader", "Debate Club", "Alsa's Cape", "Staff of Walnut" 
+    "Black Steel Set - Head",
+    "Black Steel Set - Body",
+    "Black Steel Set - Torso",
+    "Black Steel Set - Foot",
+    "Black Steel Set - Cape",
+    "Black Steel Sword",
+    "Black Steel Shield",
+    "Black Steel Sword",
+    "Ruby Necklace - ATK%",
+    "Sapphire Ring - MAGIC%",
+    "Moonlight Wand",
+    "The Invader",
+    "Debate Club",
+    "Alsa's Cape",
+    "Staff of Walnut",
 ];
 static SPECIAL_ITEMS: [&str; 15] = [
-    "Silver Sword", "Silver Helmet", "Silver Armor", "Silver Shield", "Silver Boots",
-    "Steel Sword", "Steel Shield", "Silver Helmet", "Silver Armor", "Silver Boots",
-    "Wand of Gandoff", "Hat of Agatha", "Cape of Agatha", "Boots of Agatha", "The Charger"
+    "Silver Sword",
+    "Silver Helmet",
+    "Silver Armor",
+    "Silver Shield",
+    "Silver Boots",
+    "Steel Sword",
+    "Steel Shield",
+    "Silver Helmet",
+    "Silver Armor",
+    "Silver Boots",
+    "Wand of Gandoff",
+    "Hat of Agatha",
+    "Cape of Agatha",
+    "Boots of Agatha",
+    "The Charger",
 ];
 static NORMAL_ITEMS: [&str; 10] = [
-    "Sword", "Wooden Staff", "Magic Wand", "Bow", "Free Lance",
-    "Leather Coat", "Leather Pants", "Leather Shoes", "Leather Hat", "Wedding Ring"
+    "Sword",
+    "Wooden Staff",
+    "Magic Wand",
+    "Bow",
+    "Free Lance",
+    "Leather Coat",
+    "Leather Pants",
+    "Leather Shoes",
+    "Leather Hat",
+    "Wedding Ring",
 ];
 
 #[allow(dead_code)]
@@ -39,14 +97,14 @@ enum Color {
     Purple,
     Blue,
     Cyan,
-    White
+    White,
 }
 enum Rarity {
     Legendary,
     Epic,
     Rare,
     Special,
-    Normal
+    Normal,
 }
 
 impl Rarity {
@@ -56,32 +114,45 @@ impl Rarity {
             Rarity::Epic => "Epic",
             Rarity::Rare => "Rare",
             Rarity::Special => "Special",
-            Rarity::Normal => "Normal"
+            Rarity::Normal => "Normal",
         }
     }
 }
 
 struct Item {
     name: String,
-    rarity: Rarity
+    rarity: Rarity,
 }
 impl Default for Item {
     fn default() -> Item {
-        Item { name: String::new(), rarity: Rarity::Normal }
-    }
-}
-impl Item {
-    fn show(&self) -> String {
-        match self.rarity {
-            Rarity::Legendary   => coloring_str(self.name.as_str(), Color::Yellow),
-            Rarity::Epic        => coloring_str(self.name.as_str(), Color::Purple),
-            Rarity::Rare        => coloring_str(self.name.as_str(), Color::Blue),
-            Rarity::Special     => coloring_str(self.name.as_str(), Color::Cyan),
-            Rarity::Normal      => coloring_str(self.name.as_str(), Color::White),
+        Item {
+            name: String::new(),
+            rarity: Rarity::Normal,
         }
     }
 }
+impl std::fmt::Display for Item {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let res_str = match self.rarity {
+            Rarity::Legendary => coloring_str(self.name.as_str(), Color::Yellow),
+            Rarity::Epic => coloring_str(self.name.as_str(), Color::Purple),
+            Rarity::Rare => coloring_str(self.name.as_str(), Color::Blue),
+            Rarity::Special => coloring_str(self.name.as_str(), Color::Cyan),
+            Rarity::Normal => coloring_str(self.name.as_str(), Color::White),
+        };
 
+        write!(f, "{}", res_str)
+    }
+}
+
+/// Change the color of string literal with specified color in Unix system
+/// 
+/// # Example
+/// ```rust
+/// let red_bold_hello = coloring_str("Hello!", Color::Red);
+/// assert_eq!(red_bold_hello, "\x1b[31;1mHello!\x1b[0m");
+/// println!("{}", red_bold_hello); // prints a "Hello!" message in red
+/// ```
 fn coloring_str(word: &str, color: Color) -> String {
     match color {
         Color::Red => format!("\x1b[31;1m{}\x1b[0m", word),
@@ -89,10 +160,12 @@ fn coloring_str(word: &str, color: Color) -> String {
         Color::Purple => format!("\x1b[35;1m{}\x1b[0m", word),
         Color::Blue => format!("\x1b[34;1m{}\x1b[0m", word),
         Color::Cyan => format!("\x1b[36;1m{}\x1b[0m", word),
-        Color::White => format!("\x1b[37;1m{}\x1b[0m", word)
+        Color::White => format!("\x1b[37;1m{}\x1b[0m", word),
     }
 }
 
+/// Pull `count` times and return the item being pulled along
+/// with how many resource refunded.
 fn pull(count: u8) -> (Vec<Item>, u8) {
     const MAX_OF_LEGENDARY: f64 = 0.05;
     const MAX_OF_EPIC: f64 = 0.08 + MAX_OF_LEGENDARY;
@@ -109,10 +182,12 @@ fn pull(count: u8) -> (Vec<Item>, u8) {
 
         // generate a float between 0-1
         let item_roll_num: f64 = random::float();
-        //println!("{}", item_roll_num);
+        
         if item_roll_num < MAX_OF_LEGENDARY {
             item.rarity = Rarity::Legendary;
-            item.name = random::element_in_slice(&LEGENDARY_ITEMS).unwrap().to_string();
+            item.name = random::element_in_slice(&LEGENDARY_ITEMS)
+                .unwrap()
+                .to_string();
         } else if item_roll_num < MAX_OF_EPIC {
             item.rarity = Rarity::Epic;
             item.name = random::element_in_slice(&EPIC_ITEMS).unwrap().to_string();
@@ -121,12 +196,14 @@ fn pull(count: u8) -> (Vec<Item>, u8) {
             item.name = random::element_in_slice(&RARE_ITEMS).unwrap().to_string();
         } else if item_roll_num < MAX_OF_SPECIAL {
             item.rarity = Rarity::Special;
-            item.name = random::element_in_slice(&SPECIAL_ITEMS).unwrap().to_string();
+            item.name = random::element_in_slice(&SPECIAL_ITEMS)
+                .unwrap()
+                .to_string();
         } else {
             item.rarity = Rarity::Normal;
             item.name = random::element_in_slice(&NORMAL_ITEMS).unwrap().to_string();
         }
-        
+
         // generate a float between 0 - 1 for wether refund the material used or not
         if random::float() < CHANCE_OF_REFUND {
             refund_count += 1;
@@ -134,7 +211,7 @@ fn pull(count: u8) -> (Vec<Item>, u8) {
 
         items_rolled.push(item);
     }
-    
+
     (items_rolled, refund_count)
 }
 
@@ -143,7 +220,8 @@ fn main() {
 
     let mut ticket_count: u32 = 100;
 
-    let rule_msg: String = format!("
+    let rule_msg: String = format!(
+        "
 -----------------------------------------------------------------------------
             Hello! Welcome to my very boring gotcha game.
     You have {0} {1}s left to play, each pull uses one {1}
@@ -160,7 +238,8 @@ fn main() {
         coloring_str(Rarity::Normal.stringify(), Color::White)
     );
 
-    let help_msg: String = format!("
+    let help_msg: String = format!(
+        "
     Enter 
         \"{0}\": to show game rules.
         \"{1}\": to show this help message.
@@ -168,10 +247,7 @@ fn main() {
         \"{3}\": to exit the game.
     Or enter nothing for a single pull.
         ",
-        "r",
-        "h",
-        "10",
-        "e"
+        "r", "h", "10", "e"
     );
 
     println!("{}", rule_msg);
@@ -183,24 +259,27 @@ fn main() {
 
         print!("{0} {1}(s) left: ", ticket_count, NAME_OF_TICKET);
         let _ = io::stdout().flush();
-        io::stdin().read_line(&mut input).expect("Fail to read input, try again.");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Fail to read input, try again.");
 
         match input.trim() {
-            ""   =>  {
+            "" => {
                 if ticket_count > 0 {
                     let (res, refund) = pull(1);
                     if refund > 0 {
                         println!(
                             "Pulled: [{0}] (You got a {1} back!)",
-                            res[0].show(),
+                            res[0],
                             NAME_OF_TICKET
                         );
                     } else {
                         ticket_count -= 1;
-                        println!("Pulled: [{}]", res[0].show());
+                        println!("Pulled: [{}]", res[0]);
                     }
                 } else {
-                    println!("{}",
+                    println!(
+                        "{}",
                         coloring_str(
                             format!("Sorry, you ran out of {}s", NAME_OF_TICKET).as_str(),
                             Color::Red
@@ -209,22 +288,24 @@ fn main() {
                     break 'game;
                 }
             }
-            "r"  =>  { println!("{}", rule_msg); }
-            "h"  =>  { println!("{}", help_msg); }
-            "10" =>  {
+            "r" => {
+                println!("{}", rule_msg);
+            }
+            "h" => {
+                println!("{}", help_msg);
+            }
+            "10" => {
                 if ticket_count >= 10 {
                     let (res, refund) = pull(10);
                     let mut res_str = String::new();
                     for r in &res {
-                        res_str = format!("{}, {}", res_str, &r.show());
+                        res_str = format!("{}, {}", res_str, &r);
                     }
 
                     if refund > 0 {
                         println!(
                             "Pulled: [{0}] (You got {2} {1}s back!)",
-                            res_str,
-                            NAME_OF_TICKET,
-                            refund
+                            res_str, NAME_OF_TICKET, refund
                         );
                         ticket_count = ticket_count - 10 + refund as u32;
                     } else {
@@ -232,7 +313,8 @@ fn main() {
                         ticket_count -= 10;
                     }
                 } else {
-                    println!("{}",
+                    println!(
+                        "{}",
                         coloring_str(
                             format!("You dont have enough {}s", NAME_OF_TICKET).as_str(),
                             Color::Red
@@ -240,8 +322,12 @@ fn main() {
                     );
                 }
             }
-            "e"  =>  { break 'game; },
-            _    =>  { println!("Not a valid option, try again."); }
+            "e" => {
+                break 'game;
+            }
+            _ => {
+                println!("Not a valid option, try again.");
+            }
         }
     }
 }
--- Rust/1_gotcha_game/src/rng_helper.rs
+++ Rust/1_gotcha_game/src/rng_helper.rs
@@ -1,40 +1,43 @@
 #[allow(dead_code)]
 pub mod random {
-    use rand::{thread_rng, Rng};
     use rand::seq::SliceRandom;
+    use rand::{thread_rng, Rng};
 
-    // generate a float number between 0 and 1
+    /// Generate a float number between 0 and 1.
     pub fn float() -> f64 {
         thread_rng().gen_range(0.0..1.0)
     }
 
-    // generate a float number within specific range
+    /// Generate a float number within specific range.
     pub fn float_in_range(min: f64, max: f64) -> f64 {
         if min > max {
             eprintln!(
                 "Invalid argument at Random.float_in_range(min, max). 
-                min value cannot be greater than max value. ");
+                min value cannot be greater than max value. "
+            );
             return 0.0;
         }
         thread_rng().gen_range(min..max)
     }
 
-    // generate an integer bounded by the size of a i32
+    /// Generate an integer bounded by the size of a i32.
     pub fn integer() -> i32 {
         thread_rng().gen::<i32>()
     }
 
-    // generate an interger with specific range
+    /// Generate an interger with specific range.
     pub fn integer_in_range(min: i32, max: i32) -> i32 {
         if min > max {
             eprintln!(
                 "Invalid argument at Random.integer_in_range(min, max). 
-                min value cannot be greater than max value. ");
+                min value cannot be greater than max value. "
+            );
             return 0;
         }
         thread_rng().gen_range(min..max)
     }
 
+    /// Return a random element in slice.
     pub fn element_in_slice<T>(list: &[T]) -> Option<&T> {
         list.choose(&mut thread_rng())
     }
