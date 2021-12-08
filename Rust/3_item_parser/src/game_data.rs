170e26050833f6b64b153654849e7d0a5ec8ed27
J-ZhengLi <lizhengghengj@gmail.com>
2021-12-08 17:03:23 +0800

    finishing parse from json

--- Rust/3_item_parser/src/game_data.rs
+++ Rust/3_item_parser/src/game_data.rs
@@ -1,17 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 #[allow(dead_code)]
-pub enum ItemType {
-    Armour(Item),
-    Weapon(Item),
-    Consumable,
-    Material,
-    Artifact,
-    Tradable,
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Items {
+    pub armours: Option<Vec<Item>>,
+    pub artifacts: Option<Vec<Item>>,
+    pub characters: Option<Vec<Character>>,
+    pub consumable: Option<Vec<Item>>,
+    pub materials: Option<Vec<Item>>,
+    pub tradable: Option<Vec<Item>>,
+    pub weapons: Option<Vec<Item>>,
 }
 
 #[allow(dead_code)]
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum Rarity {
     Normal,
     Special,
@@ -21,6 +23,7 @@ pub enum Rarity {
 }
 
 #[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
 enum Gender {
     Male,
     Female,
@@ -28,55 +31,70 @@ enum Gender {
 }
 
 #[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
 enum ChaType {
     Worrior,
     Ranger,
     Mage,
     Tank,
-    ExoCreature,
+    Demon,
 }
 
 #[allow(dead_code)]
-#[derive(Default)]
-struct ChaStatus {
-    level: u8,
-    max_health: u32,
-    max_stamina: u32,
-    max_mage: u32,
-    attack: u32,
-    defence: u32,
-    crit_rate: u8,
-    crit_dmg_p: u16,
-}
-
-#[allow(dead_code)]
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Default, Serialize, Deserialize, Debug)]
 struct WeaponStatus {
     level: u8,
     damage: u32,
     ex_damage: u32,
     ex_duration: u8,
     crit_rate: u8,
-    crit_dmg_p: u16
+    crit_dmg: u16,
 }
 
 #[allow(dead_code)]
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Default, Serialize, Deserialize, Debug)]
 struct ArmourStatus {
     level: u8,
     health: u32,
     defence: u32,
     damage: u32,
     stamina: u32,
-    mage: u32,
+    mana: u32,
     crit_rate: u8,
-    crit_dmg_p: u16,
-    phy_resist_p: u8,
-    mag_resist_p: u8
+    crit_dmg: u16,
+    phy_resist: u8,
+    mag_resist: u8,
 }
 
 #[allow(dead_code)]
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Position {
+    Head,
+    Chest,
+    Leg,
+    Shoe,
+    Weapon,
+    Shield,
+    Hand,
+    Necklace,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Artifact {
+    position: Position,
+    rarity: Rarity,
+    damage: u32,
+    health: u32,
+    defence: u32,
+    crit_rate: u8,
+    crit_dmg: u16,
+    phy_resist: u8,
+    mag_resist: u8,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
     name: String,
     description: String,
@@ -87,11 +105,19 @@ pub struct Item {
 }
 
 #[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Character {
     name: String,
     gender: Gender,
+    rarity: Rarity,
     description: String,
     cha_type: ChaType,
-    rarity: Rarity,
-    status: ChaStatus,
+    level: u8,
+    max_health: u32,
+    max_stamina: u32,
+    max_mana: u32,
+    crit_rate: u8,
+    crit_dmg: u16,
+    phy_resist: u8,
+    mag_resist: u8,
 }
