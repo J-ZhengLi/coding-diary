960779fb09139c672d0bdb412369916b1cdacadf
J-ZhengLi <lizhengghengj@gmail.com>
2021-12-10 18:28:18 +0800

    finishing comparing string search(maybe)

--- Rust/4_string_search_benchmark/src/main.rs
+++ Rust/4_string_search_benchmark/src/main.rs
@@ -1,52 +1,85 @@
-use std::time::Instant;
-use memchr::memmem;
 use aho_corasick::AhoCorasick;
+use memchr::memmem;
+use std::time::{Instant, Duration};
 
-fn native_search(word: &str, context: &[u8], find_all: bool) {
+fn parse_duration(duration: Duration) -> String {
+    let dur_micro = duration.as_micros();
+    if dur_micro > 1_000_000 {
+        return format!("\x1b[31;1m{} seconds\x1b[0m", duration.as_secs_f32());    // red
+    } else if dur_micro > 1_000 {
+        return format!("\x1b[33;1m{} milliseconds\x1b[0m", duration.as_millis());   // yellow
+    } else {
+        return format!("\x1b[36;1m{} microseconds\x1b[0m", dur_micro);   // cyan
+    }
+}
+
+fn native_search(words: &[&str], context: &[u8], find_all: bool) {
     let str_context = std::str::from_utf8(context).unwrap();
 
-    let timer = Instant::now();
-    if find_all {
-        let result: Vec<_> = str_context.match_indices(word).collect();
-        println!("[native] Found {} occurance of word \"{}\" in {} microsecs", result.len(), word, timer.elapsed().as_micros());
-    } else {
-        let result = str_context.find(word);
-        match result {
-            Some(n) => {
-                println!("[native] Found word \"{}\" at position {} in {} microsecs",
+    for &word in words {
+        let timer = Instant::now();
+        if find_all {
+            let result: Vec<_> = str_context.match_indices(word).collect();
+            println!(
+                "[native] Found {} occurance of word \"{}\" in {}",
+                result.len(),
+                word,
+                parse_duration(timer.elapsed())
+            );
+        } else {
+            let result = str_context.find(word);
+            match result {
+                Some(n) => {
+                    println!(
+                        "[native] Found word \"{}\" at position {} in {}",
                         word,
                         n,
-                        timer.elapsed().as_micros());
-            },
-            None => {
-                println!("[native] Did not find word \"{}\", time elapsed: {} microsecs",
+                        parse_duration(timer.elapsed())
+                    );
+                }
+                None => {
+                    println!(
+                        "[native] Did not find word \"{}\", time elapsed: {}",
                         word,
-                        timer.elapsed().as_micros());
+                        parse_duration(timer.elapsed())
+                    );
+                }
             }
         }
     }
 }
 
-fn memmem_search(word: &str, context: &[u8], find_all: bool) {
-    // start timer
-    let timer = Instant::now();
-
-    if find_all {
-        let mem_it = memmem::find_iter(context, word);
-        println!("[memmem] Found {} occurance of word \"{}\" in {} microsecs", mem_it.count(), word, timer.elapsed().as_micros());
-    } else {
-        let finder = memmem::Finder::new(word);
-        match finder.find(context) {
-            Some(n) => {
-                println!("[memmem] Found word \"{}\" at position {} in {} microsecs",
+fn memmem_search(words: &[&str], context: &[u8], find_all: bool) {
+    for &word in words {
+        // start timer
+        let timer = Instant::now();
+        
+        if find_all {
+            let mem_it = memmem::find_iter(context, word);
+            println!(
+                "[memmem] Found {} occurance of word \"{}\" in {}",
+                mem_it.count(),
+                word,
+                parse_duration(timer.elapsed())
+            );
+        } else {
+            let finder = memmem::Finder::new(word);
+            match finder.find(context) {
+                Some(n) => {
+                    println!(
+                        "[memmem] Found word \"{}\" at position {} in {}",
                         word,
                         n,
-                        timer.elapsed().as_micros());
-            },
-            None => {
-                println!("[memmem] Did not find word \"{}\", time elapsed: {} microsecs",
+                        parse_duration(timer.elapsed())
+                    );
+                }
+                None => {
+                    println!(
+                        "[memmem] Did not find word \"{}\", time elapsed: {}",
                         word,
-                        timer.elapsed().as_micros());
+                        parse_duration(timer.elapsed())
+                    );
+                }
             }
         }
     }
@@ -60,20 +93,29 @@ fn aho_corasick_seach(words: &[&str], context: &[u8], find_all: bool) {
 
     if find_all {
         let result_count = aho.find_iter(context).count();
-        println!("[aho-corasick] Found {} occurance of words \"{:?}\" in {} microsecs", result_count, words, timer.elapsed().as_micros());
+        println!(
+            "[aho-corasick] Found {} occurance of words \"{:?}\" in {}",
+            result_count,
+            words,
+            parse_duration(timer.elapsed())
+        );
     } else {
         let found = aho.find(context);
         match found {
             Some(m) => {
-                println!("[aho-corasick] Found leftmost match \"{}\" at position {} in {} microsecs",
-                        std::str::from_utf8(&context[m.start()..m.end()]).unwrap(),
-                        m.start(),
-                        timer.elapsed().as_micros());
-            },
+                println!(
+                    "[aho-corasick] Found leftmost match \"{}\" at position {} in {}",
+                    std::str::from_utf8(&context[m.start()..m.end()]).unwrap(),
+                    m.start(),
+                    parse_duration(timer.elapsed())
+                );
+            }
             None => {
-                println!("[aho-corasick] Did not find any word in list \"{:?}\", time elapsed: {} microsecs",
-                        words,
-                        timer.elapsed().as_micros());
+                println!(
+                    "[aho-corasick] Did not find any word in list \"{:?}\", time elapsed: {}",
+                    words,
+                    parse_duration(timer.elapsed())
+                );
             }
         }
     }
@@ -88,18 +130,59 @@ fn main() {
     // a non-existing word to be test
     let non_exist_word = "uwuwwwwxoxouuu";
 
-    println!("\n==================== Finding non-existing string (worst-case) ====================");
-    native_search(non_exist_word, full_text, true);
-    memmem_search(non_exist_word, full_text, true);
+    let three_words = &["tHGp3FEBx7", "skJCoFTN93", "fNx8YU"];
+
+    let ten_words = &["Hq6JYTKV", "7SC4yc4Vt", "58PjWm",
+                                "3tRoIEot", "97OUJh", "i6xiuKP",
+                                "fbkB0PB", "RUfmE4la", "H46v",
+                                "A_NON_EXISTING_WORD"];
+
+    println!(
+        "\n==================== Finding non-existing string (worst-case) ===================="
+    );
+    native_search(&[non_exist_word], full_text, true);
+    memmem_search(&[non_exist_word], full_text, true);
     aho_corasick_seach(&[non_exist_word], full_text, true);
 
-    println!("\n=================== Finding first occurrance of a single string ===================");
-    native_search(word_1, full_text, false);
-    memmem_search(word_1, full_text, false);
+    println!(
+        "\n=================== Finding first occurrance of a single string ==================="
+    );
+    native_search(&[word_1], full_text, false);
+    memmem_search(&[word_1], full_text, false);
     aho_corasick_seach(&[word_1], full_text, false);
 
-    println!("\n==================== Finding all occurrance of a single string ====================");
-    native_search(word_1, full_text, true);
-    memmem_search(word_1, full_text, true);
+    println!(
+        "\n==================== Finding all occurrance of a single string ===================="
+    );
+    native_search(&[word_1], full_text, true);
+    memmem_search(&[word_1], full_text, true);
     aho_corasick_seach(&[word_1], full_text, true);
+
+    println!(
+        "\n==================== Finding first occurrance of three strings ===================="
+    );
+    native_search(three_words, full_text, false);
+    memmem_search(three_words, full_text, false);
+    aho_corasick_seach(three_words, full_text, false);
+
+    println!(
+        "\n==================== Finding all occurrance of three strings ===================="
+    );
+    native_search(three_words, full_text, true);
+    memmem_search(three_words, full_text, true);
+    aho_corasick_seach(three_words, full_text, true);
+
+    println!(
+        "\n==================== Finding first occurrance of ten strings ===================="
+    );
+    native_search(ten_words, full_text, false);
+    memmem_search(ten_words, full_text, false);
+    aho_corasick_seach(ten_words, full_text, false);
+
+    println!(
+        "\n==================== Finding all occurrance of ten strings ===================="
+    );
+    native_search(ten_words, full_text, true);
+    memmem_search(ten_words, full_text, true);
+    aho_corasick_seach(ten_words, full_text, true);
 }
