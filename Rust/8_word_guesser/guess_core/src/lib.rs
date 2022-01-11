f7a2be7a3c1f548b3d02eb0b5e5c715d58ec78e1
J-ZhengLi <lizhengghengj@gmail.com>
2022-01-11 20:10:20 +0800

    add guess_core

---
+++ Rust/8_word_guesser/guess_core/src/lib.rs
@@ -0,0 +1,50 @@
+cfg_if::cfg_if! {
+    if #[cfg(feature = "web")] {
+        use reqwest;
+        use tokio::runtime::Builder;
+
+        const SOURCE_URL: &str = "https://www.insult.wiki/list-of-insults";
+
+        async fn load_from_url(url: &str) -> String {
+            let body = reqwest::get(url)
+                .await.expect("Could not connect to given url.")
+                .text()
+                .await.expect("Fail to get the full response text.");
+
+            body
+        }
+    }
+}
+
+pub fn get_word_list() -> Vec<String> {
+    #[cfg(feature = "web")]
+    let full_text = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async { load_from_url(SOURCE_URL).await });
+
+    #[cfg(not(feature = "web"))]
+    let full_text: String = include_str!("../../res/text/insult_list").to_string();
+
+    let result: Vec<String> = Vec::from_iter(full_text.split_whitespace().map(String::from));
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_word_list;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn fetch_raw_html() {
+        let result = get_word_list();
+        println!("{:?}", result);
+    }
+}
--- Rust/8_word_guesser/src/main.rs
+++ Rust/8_word_guesser/src/main.rs
@@ -6,13 +6,18 @@
 //! one for reading user input,
 //! and one for processing the guesses.
 
+use guess_core::get_word_list;
+use std::fmt::Display;
 use std::io::{self, stdin, Read, Write};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc, Mutex,
 };
 use std::{thread, time::Duration};
-use termion::raw::IntoRawMode;
+use termion::{
+    cursor::{self, DetectCursorPos},
+    raw::IntoRawMode,
+};
 
 fn add_char(mutex_s: &Mutex<String>, ch: char) {
     let mut old_s = mutex_s
@@ -30,41 +35,73 @@ fn pop_char(mutex_s: &Mutex<String>) {
     (*old_s).pop();
 }
 
+fn make_guess(input: &str) -> String {
+    let prefix: String = "Did you mean: ".to_string();
+    let postfix: String = "? wow...".to_string();
+    format!("\x1b[33;1m{}\"{}\"{}\x1b[0m", prefix, input, postfix)
+}
+
+/// Using `print!` macro to print given content, but without expend the macro everywhere.
+#[cold]
+#[inline(never)]
+fn basic_print<T: Display>(s: T) {
+    print!("{}", s);
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
     let typed_string: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
     let typed_string_clone = typed_string.clone();
     let quit_pressed: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
     let quit_pressed_clone = quit_pressed.clone();
-    println!("\nType anything, I will guess what's in your mind~\nPress [ESC] to quit.");
+    let cursor_pos = Arc::new(Mutex::new((0_u16, 0_u16)));
+    let cursor_pos_clone = cursor_pos.clone();
+
+    // Switch the terminal to raw mode
+    let mut stdout = io::stdout()
+        .into_raw_mode()
+        .expect("Fail to change terminal mode.");
+
+    basic_print("Loading Resources...");
+    let _words = get_word_list();
+    print!("\r{}", termion::clear::CurrentLine);
+    io::stdout().flush().unwrap();
+
+    basic_print("\nType anything, I will guess what's in your mind~\n\r\
+                Press [ESC] to quit.\n\n\r");
 
     // Thread to read input and prints it out on console
     let input_handler = thread::spawn(move || {
         let mut buf = [0_u8; 1];
-        let mut stdout = io::stdout().into_raw_mode().unwrap();
         while stdin().read(&mut buf).expect("Fail to read input.") == 1 {
             let key = buf[0];
             match buf[0] {
                 27 => {
                     quit_pressed.store(true, Ordering::Relaxed);
-                    write!(stdout, "\n\rLeaving...\n\r").unwrap();
+                    basic_print("\n\rLeaving...\n\r");
                     break;
                 }
                 127 => {
-                    write!(
-                        stdout,
-                        "{}{}",
-                        termion::cursor::Left(1),
-                        termion::clear::AfterCursor
-                    )
-                    .unwrap();
+                    print!("{}{}", cursor::Left(1), termion::clear::AfterCursor);
                     pop_char(&typed_string);
                     io::stdout().flush().unwrap();
+
+                    // save cursor pos
+                    let mut pos = cursor_pos.lock().unwrap();
+                    *pos = stdout
+                        .cursor_pos()
+                        .expect("Fail to get current cursor position.");
                 }
-                x if key <= 126 && key >= 32 => {
+                x if (32..=126).contains(&key) => {
                     let ch: char = x as char;
-                    write!(stdout, "{}", ch).unwrap();
+                    basic_print(ch);
                     add_char(&typed_string, ch);
-                    io::stdout().flush().unwrap();
+
+                    // save cursor pos
+                    let mut pos = cursor_pos.lock().unwrap();
+                    *pos = stdout
+                        .cursor_pos()
+                        .expect("Fail to get current cursor position.");
                 }
                 _ => {}
             }
@@ -79,7 +116,28 @@ fn main() {
         while !quit_pressed_clone.load(Ordering::Relaxed) {
             let current_input = typed_string_clone.lock().unwrap().to_string();
             if input_buff != current_input {
-                print!("{}", current_input);
+                let o_cursor_pos = cursor_pos_clone.lock().unwrap();
+                let random_funny_text = if current_input.is_empty() {
+                    String::new()
+                } else {
+                    make_guess(&current_input)
+                };
+
+                // The logic of this print macro is:
+                // Move the cursor to the begining of current line - "\r"
+                // Move the cursor up by one line - "cursor::Up(1)"
+                // Clear what remains on this upper line
+                // print guess content
+                // move the cursor back to the bottom line (which saved in input handle)
+                print!(
+                    "\r{}{}{}{}",
+                    cursor::Up(1),
+                    termion::clear::CurrentLine,
+                    random_funny_text,
+                    cursor::Goto(o_cursor_pos.0, o_cursor_pos.1)
+                );
+                // move cursor back
+
                 input_buff = current_input;
                 io::stdout().flush().unwrap();
             }
