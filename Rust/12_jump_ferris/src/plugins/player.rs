d05e3af615b1933df897f2a91d398dfb1fc36d3e
J-ZhengLi <lizheng135@huawei.com>
2022-10-14 18:35:53 +0800

    [`project_14`]: - add jump utility and ground detection;

--- Rust/12_jump_ferris/src/plugins/player.rs
+++ Rust/12_jump_ferris/src/plugins/player.rs
@@ -7,8 +7,29 @@ use super::background::BG_SCALE;
 
 const FERRIS_SIZE_X: f32 = 34.0;
 const FERRIS_SIZE_Y: f32 = 21.0;
+const MIN_JUMP_FORCE: f32 = 80.;
+const MAX_JUMP_FORCE: f32 = 260.;
+const JUMP_CHARGE_SCALE: f32 = 180.;
 
 pub struct PlayerPlugin;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct MoveSpeed(f32);
+
+#[derive(Component)]
+struct JumpForce(f32);
+
+#[derive(Component, Clone, Copy)]
+struct Grounded(bool);
+
+impl Grounded {
+    fn set(&mut self, is_grounded: bool) {
+        self.0 = is_grounded;
+    }
+}
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
@@ -22,7 +43,12 @@ impl Plugin for PlayerPlugin {
             SystemSet::on_update(GameState::InitPlayer).with_system(init_player_animations),
         )
         .add_system_set(SystemSet::on_update(GameState::LoadingPlayer).with_system(init_player))
-        .add_system_set(SystemSet::on_update(GameState::Running).with_system(anim_player));
+        .add_system_set(
+            SystemSet::on_update(GameState::Running)
+                .with_system(anim_player)
+                .with_system(player_movement)
+                .with_system(ground_detection),
+        );
     }
 }
 
@@ -35,7 +61,8 @@ fn init_player_animations(
     info!("initializing ferris...");
 
     let ferris_idle: Handle<Image> = assets.load("ferris-idle.png");
-    let t_atlas = TextureAtlas::from_grid(ferris_idle, Vec2::new(FERRIS_SIZE_X, FERRIS_SIZE_Y), 6, 1);
+    let t_atlas =
+        TextureAtlas::from_grid(ferris_idle, Vec2::new(FERRIS_SIZE_X, FERRIS_SIZE_Y), 6, 1);
     let atlas_handle = texture_atlases.add(t_atlas);
     cmd.insert_resource(atlas_handle);
 
@@ -54,8 +81,18 @@ fn init_player(
         transform: Transform::from_xyz(-80., -200., Layer::Characters.into()).with_scale(BG_SCALE),
         ..Default::default()
     })
+    .insert(Player)
+    .insert(MoveSpeed(0.5))
     .insert(Collider::cuboid(FERRIS_SIZE_X / 2., FERRIS_SIZE_Y / 2.))
+    .insert(ActiveEvents::COLLISION_EVENTS)
     .insert(RigidBody::Dynamic)
+    // an external impulse with no magnitude
+    .insert(ExternalImpulse {
+        impulse: Vec2::new(0., 0.),
+        torque_impulse: 0.0,
+    })
+    .insert(JumpForce(MAX_JUMP_FORCE))
+    .insert(Grounded(false))
     .insert(AnimationTimer(Timer::from_seconds(0.1, true)));
 
     state
@@ -78,6 +115,62 @@ fn anim_player(
         if timer.just_finished() {
             let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
             sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
+}
+
+fn player_movement(
+    time: Res<Time>,
+    keys: Res<Input<KeyCode>>,
+    mut player: Query<
+        (
+            &mut Transform,
+            &MoveSpeed,
+            &mut ExternalImpulse,
+            &mut JumpForce,
+            &Grounded,
+        ),
+        With<Player>,
+    >,
+) {
+    let (mut tr, speed, mut impulse, mut j_force, grounded) = player
+        .get_single_mut()
+        .expect("no player was added to the scene");
+    // freeze rotation
+    tr.rotation = Quat::IDENTITY;
+
+    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        tr.translation.x -= speed.0;
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        tr.translation.x += speed.0;
+    }
+    if grounded.0 && keys.pressed(KeyCode::Space) {
+        if j_force.0 < MAX_JUMP_FORCE {
+            j_force.0 += JUMP_CHARGE_SCALE * time.delta_seconds();
+        }
+    }
+    if grounded.0 && keys.just_released(KeyCode::Space) {
+        impulse.impulse = Vec2::new(0., j_force.0);
+        j_force.0 = MIN_JUMP_FORCE;
+    }
+}
+
+fn ground_detection(
+    mut collision_events: EventReader<CollisionEvent>,
+    mut player: Query<(&mut Grounded, Entity), With<Player>>,
+) {
+    let (mut grounded, entity) = player
+        .get_single_mut()
+        .expect("no player was added to the scene");
+
+    for collision_event in collision_events.iter() {
+        if let CollisionEvent::Started(ent_a, ent_b, _) = collision_event {
+            if ent_a.id() == entity.id() || ent_b.id() == entity.id() {
+                grounded.set(true);
+            }
+        } else {
+            grounded.set(false);
         }
     }
 }
